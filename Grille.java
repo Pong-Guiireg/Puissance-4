85851ec95652c98f5576c7f4d015fb278d385293
evalenti <valentin.epaud@ynov.com>
2025-02-14 08:54:56 +0100

    add victory to players

--- Grille.java
+++ Grille.java
@@ -93,6 +93,7 @@ public class Grille {
                     ClearTerminal();
                     temp = true;
                     gameStarted = false;
+                    currentPlayers.nombreDeVictoire++;
                 }else {
                     System.exit(0);
                 }
